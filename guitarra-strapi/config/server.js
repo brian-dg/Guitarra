3d5cd9228daed2f43cc5f3a630c2a3541ec81c70
brian-dg <brian_rc6@hotmail.com>
2022-01-10 12:44:12 -0300

    agregando strapi

---
+++ guitarra-strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '27251f98709a7298711dc367c9d22328'),
+    },
+  },
+});
---
+++ guitarra-strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '51256a64-7a85-40cb-87fb-32a2fcd197b8'
+};
