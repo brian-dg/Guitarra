74e8ac70c3f902d2e7032775db702bd52974fdf9
brian-dg <brian_rc6@hotmail.com>
2022-01-12 10:32:09 -0300

    Consultado a la api con GetStaticProps

--- pages/blog.js
+++ pages/blog.js
@@ -1,18 +1,9 @@
 import Layout from "../components/Layout";
-import { useEffect } from "react";
 
-const Blog = () => {
 
-    useEffect(() => {
-        const consultarAPI = async () => {
-            const url = 'http://localhost:1337/blogs';
-            const respuesta = await fetch(url)
-            const resultado = await respuesta.json()
+const Blog = ({resultado}) => {
 
-            console.log(resultado);
-        }
-        consultarAPI();
-    },[])
+    console.log(resultado)
     return(
         <Layout 
         pagina="Blog"
@@ -21,8 +12,30 @@ const Blog = () => {
         </Layout>
     );
 }
-//probando
+/* Esta funcion construye cada vez qe se dirige a una pagina lo cual genera un uso muy intensivo
+Construye una respuesta diferente  cada vez qe va a la api.
 export async function getServerSideProps() {
-    
+    const url = 'http://localhost:1337/blogs';
+    const respuesta = await fetch(url)
+    const resultado = await respuesta.json()
+
+    return {
+        props: {
+            resultado
+        }
+    }
+}*/
+
+//Usar cuando el contenido es estatico. 
+export async function getStaticProps() {
+    const url = 'http://localhost:1337/blogs';
+    const respuesta = await fetch(url)
+    const resultado = await respuesta.json()
+
+    return {
+        props: {
+            resultado
+        }
+    }
 }
 export default Blog;
